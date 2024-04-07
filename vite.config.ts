efab68d6fcf7dbd6a06d419875b625e6cf10f1fa
mAmineChniti <emin.chniti@esprit.tn>
2024-04-07 14:26:10 +0100

    ./assets -> assets

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         ,
         resolve: {
                 alias: {
-                        '@assets': resolve(__dirname, './assets'),
+                        '@assets': resolve(__dirname, 'assets'),
                 },
         },
         server: {
