0f4562f07e72af1d712be9dd64cbd153f6b4759b
mAmineChniti <emin.chniti@esprit.tn>
2024-04-29 19:12:23 +0100

    added url plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
+import url from '@rollup/plugin-url';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), url()],
   resolve: {
     alias: {
       '@assets': resolve(__dirname, '**/assets'),
